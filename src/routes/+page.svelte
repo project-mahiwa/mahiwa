9b298a357e006a32735d3bd0f81f980889304532
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-11-30 22:11:21 +0900

    feat 最低限の実装

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Greet from '../lib/Greet.svelte';
+	import { _ } from 'svelte-i18n';
 </script>
 
-<h1 class="text-3xl">Welcome to SvelteKit</h1>
+<h1 class="text-3xl">{$_('top.top')}</h1>
 <Greet />
