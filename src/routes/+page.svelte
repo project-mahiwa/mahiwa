cc509b285e9fe163340be5f3d575c02ca517d59e
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-12-01 00:13:36 +0900

    feat いったん仮組

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,22 @@
 <script>
-	import Greet from '../lib/Greet.svelte';
+	import Greet from '$lib/native/Greet.svelte';
 	import { _ } from 'svelte-i18n';
+	import MahiwaTitle from '$lib/components/attom/text/MahiwaTitle.svelte';
+	import NormalButton from '$lib/components/attom/button/NormalButton.svelte';
 </script>
 
-<h1 class="text-3xl text-yellow">{$_('top.top')}</h1>
-<Greet />
+<div class="pt-20 flex justify-center items-center flex-col">
+	<MahiwaTitle />
+	<div class="pt-12 flex justify-center imtes-center flex-wrap">
+		<NormalButton className="p-4" title={$_('top.button.flash')} url="/flash" />
+		<NormalButton className="p-4" title={$_('top.button.generate')} url="/generate" />
+		<NormalButton
+			className="p-4"
+			isInternal={false}
+			title={$_('top.button.document')}
+			url="/document"
+		/>
+	</div>
+
+	<Greet />
+</div>
