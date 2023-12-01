eb517646fbe00cd75401ac835731ff1eebaf44a3
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-12-01 16:10:04 +0900

    feat font導入

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				'kiwi-maru': ['Kiwi Maru'],
+				'dot-gothic16': ['DotGothic16']
+			},
 			colors: {
 				white: 'var(--white)',
 				black: 'var(--black)',
