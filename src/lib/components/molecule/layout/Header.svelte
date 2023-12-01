1d90eccd6b56b6457adbd7754a54fd1e0006a82a
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-12-01 16:27:02 +0900

    feat Headerきれいにする

--- src/lib/components/molecule/layout/Header.svelte
+++ src/lib/components/molecule/layout/Header.svelte
@@ -1,5 +1,5 @@
 <script>
-	import MahiwaTitle from '$lib/components/attom/text/MahiwaTitle.svelte';
+	import MahiwaTitle from '$lib/components/atom/text/MahiwaTitle.svelte';
 	import { title } from '$lib/stores/header';
 	let title_value = '';
 	title.subscribe((value) => {
@@ -7,8 +7,16 @@
 	});
 </script>
 
-<header class="flex w-full h-20 border-2 border-b border-gray">
-	<div class="w-1/3"><a href="/">戻るボタン</a></div>
-	<div class="w-1/3">{title_value}</div>
-	<div class="w-1/3"><MahiwaTitle /></div>
+<header class="flex items-center w-full h-16 border-b-2 border-gray">
+	<div class="w-1/3 pl-2">
+		<a class="" href="/">
+			<div class="flex justify-center items-center bg-black text-white w-8 h-8 rounded-full">
+				<p class="text-xl">←</p>
+			</div>
+		</a>
+	</div>
+	<div class="w-1/3 text-2xl text-center">{title_value}</div>
+	<div class="w-1/3 flex justify-end pr-4">
+		<MahiwaTitle titleClass="text-xl md:text-xl" />
+	</div>
 </header>
