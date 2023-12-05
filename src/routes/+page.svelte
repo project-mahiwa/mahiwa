f521d3ab2a467953ddaeb681eaf40cc5108088b9
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-12-05 22:40:45 +0900

    feat マシン情報をトップで表示しておくようにする

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,27 @@
 	import { _ } from 'svelte-i18n';
 	import MahiwaTitle from '$lib/components/atom/text/MahiwaTitle.svelte';
 	import NormalButton from '$lib/components/atom/button/NormalButton.svelte';
+	import { type, arch, locale, platform, version } from '@tauri-apps/api/os';
+	import { onMount } from 'svelte';
+	let archName = '';
+	let localeName = '';
+	let platformName = '';
+	let versionName = '';
+	let typeName = '';
+	onMount(async () => {
+		archName = await arch();
+		typeName = await type();
+		localeName = (await locale()) ?? 'not set';
+		platformName = await platform();
+		versionName = await version();
+	});
 </script>
 
 <div class="w-full h-full flex justify-center items-center flex-col">
 	<MahiwaTitle titleClass="text-2xl md:text-3xl" />
+	<div class="mt-2">
+		<p>{`⚙ ${archName}(${typeName}) | 🌐 ${localeName} | 💻 ${platformName}(${versionName})  `}</p>
+	</div>
 	<div class="pt-12 flex justify-center imtes-center flex-wrap">
 		<NormalButton className="p-4" title={$_('flash.title')} url="/flash" />
 		<NormalButton className="p-4" title={$_('generator.title')} url="/generator" />
