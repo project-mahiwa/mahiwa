551276a14061a96a2c025a8a4c7b3145703e4b2f
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-12-06 14:29:16 +0900

    wip serialportライブラリで自前serial通信

--- src-tauri/src/flash/serial.rs
+++ src-tauri/src/flash/serial.rs
@@ -1,21 +1,38 @@
-use std::io::{BufRead, BufReader};
-use std::process::{Command, Stdio};
+use serialport::prelude::*;
+use std::io::{self, Read};
+use std::time::Duration;
 use tauri::Window;
+
 #[tauri::command]
 pub fn serial(window: Window) -> Result<(), ()> {
-    /*
-     * pio monitorで見る
-     */
-    let mut child = Command::new("pio")
-        .args(["device", "monitor", "-b", "115200"])
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("Failed to pio device monitor");
-    let stdout = child.stdout.take().unwrap();
-    // 1行ずつ結果を取る
-    let reader = BufReader::new(stdout);
-    for line in reader.lines() {
-        window.emit("btf-flash-prgoress", line.unwrap()).unwrap();
+    let port_name = "/dev/ttyUSB0";
+    let baud_rate = 9600;
+
+    let settings = SerialPortSettings {
+        baud_rate,
+        timeout: Duration::from_secs(1),
+        ..Default::default()
+    };
+
+    match serialport::open_with_settings(&port_name, &settings) {
+        Ok(mut port) => {
+            let mut serial_buf: Vec<u8> = vec![0; 1000];
+            println!("Reading data from {} at {} baud:", &port_name, &baud_rate);
+            loop {
+                match port.read(serial_buf.as_mut_slice()) {
+                    Ok(t) => io::stdout().write_all(&serial_buf[..t]).unwrap(),
+                    Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+                    Err(e) => eprintln!("{:?}", e),
+                }
+            }
+        }
+        Err(e) => {
+            eprintln!("Failed to open \"{}\". Error: {}", port_name, e);
+            ::std::process::exit(1);
+        }
     }
+    // for line in reader.lines() {
+    //     window.emit("btf-flash-prgoress", line.unwrap()).unwrap();
+    // }
     return Ok(());
 }
